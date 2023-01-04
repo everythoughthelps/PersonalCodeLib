a06a555ff1d4dd4f6da6985b64182c93b691f5d5
everythoughthelps <panm9@mail2.sysu.edu.cn>
2023-01-04 22:34:12 +0800

    add video stream process script

---
+++ video_stream_process.py
@@ -0,0 +1,45 @@
+import numpy as np
+from torchvision.transforms import ToTensor
+import cv2
+import subprocess
+
+rtsp = "rtsp://172.19.33.6:554"
+rtmp = 'rtmp://localhost:1935/mylive/test'
+
+# 读取视频并获取属性
+cap = cv2.VideoCapture(rtsp)
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+sizeStr = str(size[0]) + 'x' + str(size[1])
+
+command = ['ffmpeg',
+           '-y', '-an',
+           '-f', 'rawvideo',
+           '-vcodec', 'rawvideo',
+           '-pix_fmt', 'bgr24',
+           '-s', sizeStr,
+           '-r', '25',
+           '-i', '-',
+           '-c:v', 'libx264',
+           '-pix_fmt', 'yuv420p',
+           '-preset', 'ultrafast',
+           '-f', 'flv',
+           rtmp]
+
+pipe = subprocess.Popen(command
+                        , shell=False
+                        , stdin=subprocess.PIPE
+                        )
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        '''
+		对frame进行识别处理
+		'''
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        pipe.stdin.write(frame.tostring())
+
+cap.release()
+pipe.terminate()
+
